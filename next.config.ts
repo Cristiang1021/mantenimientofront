c14733f83323d2c4704af67877e2f201119e418b
Cristiang1021 <andregonza46@hotmail.com>
2025-05-12 19:32:21 -0500

    Final final

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Activa el modo estricto de React
   images: {
-    domains: ["backendmantenimiento.onrender.com"], // Permite cargar imágenes desde el backend
+    domains: ["https://backendmantenimiento-nxyz.onrender.com"], // Permite cargar imágenes desde el backend
   },
   env: {
     API_BASE_URL: process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:3001" // URL en desarrollo
-      : "https://backendmantenimiento.onrender.com", // URL en producción
+      ? "https://backendmantenimiento-nxyz.onrender.com" // URL en desarrollo
+      : "https://backendmantenimiento-nxyz.onrender.com", // URL en producción
   },
 };
 
